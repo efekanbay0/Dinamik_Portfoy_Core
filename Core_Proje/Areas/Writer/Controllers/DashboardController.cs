928dbf2f7ab7d4075dd4f252865f7ead47119cc5
EfekanBay <bayefekan@outlook.com>
2023-04-28 16:05:58 +0300

    Hava Durumu Apisi Cekildi

--- Core_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
@@ -22,6 +23,12 @@ namespace Core_Proje.Areas.Writer.Controllers
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.v = values.Name + " " + values.Surname;
 
+            //Weather API
+            string api = "544f7a8f8358651f1b7669485f483ba5";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=ankara&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             Context c = new Context();
             ViewBag.v1 = 0; 
             ViewBag.v2 = c.announcements.Count();
@@ -30,4 +37,4 @@ namespace Core_Proje.Areas.Writer.Controllers
             return View();
         }
     }
-}
+}/* https://api.openweathermap.org/data/2.5/weather?q=ankara&mode=xml&lang=tr&units=metric&appid=544f7a8f8358651f1b7669485f483ba5 */
