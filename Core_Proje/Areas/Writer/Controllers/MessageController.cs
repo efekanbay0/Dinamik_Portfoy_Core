29bd2a24edd1c4e093edba2f81db5fbd8ddc908d
EfekanBay <bayefekan@outlook.com>
2023-05-01 20:45:10 +0300

    Gelen ve Giden Mesajları Listeleme

--- Core_Proje/Areas/Writer/Controllers/MessageController.cs
+++ Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -19,12 +19,21 @@ namespace Core_Proje.Areas.Writer.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index(string p)
+        public async Task<IActionResult> ReceiverMessage(string p)
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             p = values.Email;
             var messageList = writermessageManager.GetListReceiverMessage(p); 
             return View(messageList);
         }
+
+        public async Task<IActionResult> SenderMessage(string p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            p = values.Email;
+            var messageList = writermessageManager.GetListSenderMessage(p);
+            return View(messageList);
+        }
+
     }
 }
